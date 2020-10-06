03e692bb3ece6de8872089a0ce8842133f476278
Jorge Parada Fernandez <jwuhon@gmail.com>
2020-10-06 13:01:41 +0200

    ADD: Llamada a Activity

--- app/src/main/java/com/ctbarbanza/gupyou/LoginActivity.java
+++ app/src/main/java/com/ctbarbanza/gupyou/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.ctbarbanza.gupyou;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,10 +11,12 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LoginActivity extends AppCompatActivity {
 
 
+    private LoginActivity ctx;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        ctx = this;
         setContentView(R.layout.activity_login);
 
         Button btnRegistro = findViewById(R.id.act_login_register_btn);
@@ -23,6 +26,8 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Cuando Alguien Hace Click en el Boton
+                Intent intent = new Intent(ctx, RegisterActivity.class);
+                startActivity(intent);
             }
         });
 
