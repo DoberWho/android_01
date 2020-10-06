3b655c37942deb215b246259c0c497a7607ba62e
Jorge Parada Fernandez <jwuhon@gmail.com>
2020-10-06 12:41:36 +0200

    ADD: Boton

--- app/src/main/java/com/ctbarbanza/gupyou/LoginActivity.java
+++ app/src/main/java/com/ctbarbanza/gupyou/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.ctbarbanza.gupyou;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+
+        Button btnRegistro = findViewById(R.id.act_login_register_btn);
+
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Cuando Alguien Hace Click en el Boton
+            }
+        });
 
     }
 
