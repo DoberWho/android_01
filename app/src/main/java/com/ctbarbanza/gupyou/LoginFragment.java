6e3f625c69ed45fab1dbddb35ade9c2d8a357625
Jorge Parada Fernandez <jwuhon@gmail.com>
2020-10-08 13:58:35 +0200

    ADD: AuthActivity + Fragments

---
+++ app/src/main/java/com/ctbarbanza/gupyou/LoginFragment.java
@@ -0,0 +1,35 @@
+package com.ctbarbanza.gupyou;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.frg_login, container, false);
+        Button btn = v.findViewById(R.id.frg_login_action_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+        return v;
+    }
+}
