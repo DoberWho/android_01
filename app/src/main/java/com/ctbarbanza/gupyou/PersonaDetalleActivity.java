ae5e789e0d7390016cebf12a54fdc84f4a27d5b7
Jorge Parada Fernandez <jwuhon@gmail.com>
2020-10-29 13:10:50 +0100

    ADD: Como Rellenar la información de un Widget TextView

--- app/src/main/java/com/ctbarbanza/gupyou/PersonaDetalleActivity.java
+++ app/src/main/java/com/ctbarbanza/gupyou/PersonaDetalleActivity.java
@@ -3,12 +3,17 @@ package com.ctbarbanza.gupyou;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.ctbarbanza.gupyou.models.Persona;
 
 public class PersonaDetalleActivity extends AppCompatActivity {
 
     public static final String BUNDLE_PERSONA = "persona";
+
+    private Persona persona;
+
+    private TextView txtName, txtLastName, txtFacebook, txtGithub, txtGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +22,29 @@ public class PersonaDetalleActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         if (extras != null){
-            Persona persona = (Persona) extras.get(BUNDLE_PERSONA);
+            persona = (Persona) extras.get(BUNDLE_PERSONA);
         }
+
+        initView();
+        updateData();
+
+    }
+
+    private void updateData() {
+        txtName.setText(persona.getName());
+        txtLastName.setText(persona.getLastName());
+        txtFacebook.setText(persona.getFaceook());
+        txtGithub.setText(persona.getGithub());
+        txtGoogle.setText(persona.getGmail());
+    }
+
+    private void initView(){
+        txtName = findViewById(R.id.act_persona_detalle_name_txt);
+        txtLastName = findViewById(R.id.act_persona_detalle_name_txt);
+
+        txtFacebook = findViewById(R.id.act_persona_detalle_name_txt);
+        txtGithub = findViewById(R.id.act_persona_detalle_name_txt);
+        txtGoogle = findViewById(R.id.act_persona_detalle_name_txt);
 
     }
 }
