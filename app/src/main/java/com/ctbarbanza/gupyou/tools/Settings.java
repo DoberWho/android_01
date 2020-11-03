662419f9eff6ce3f1e0ed4d186fb4a3b55028ed8
Jorge Parada Fernandez <jwuhon@gmail.com>
2020-11-03 12:00:17 +0100

    ADD: SharedPreferences => Con Float+String NOTA: Importante

---
+++ app/src/main/java/com/ctbarbanza/gupyou/tools/Settings.java
@@ -0,0 +1,46 @@
+package com.ctbarbanza.gupyou.tools;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.ctbarbanza.gupyou.R;
+
+class Settings {
+
+    private SharedPreferences sharedPref;
+
+    private Settings(Context ctx){
+        sharedPref = ctx.getSharedPreferences("configuracion", Context.MODE_PRIVATE);
+    }
+    private static Settings instance;
+
+    public static Settings init(Context ctx){
+        if (instance == null){
+            instance = new Settings(ctx);
+        }
+        return instance;
+    }
+
+    public void save(String key, String value){
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public String getString(String key){
+        return  sharedPref.getString(key,"");
+    }
+
+    public void save(String key, Float value){
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putFloat(key, value);
+        editor.commit();
+    }
+
+    public Float getFloat(String key){
+        float valorPorDefecto = 0;
+        return  sharedPref.getFloat(key,valorPorDefecto);
+    }
+
+
+}
