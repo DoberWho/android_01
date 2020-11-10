9fc449e23caab813f5a5f67cd7135da7cc2928b8
Jorge Parada Fernandez <jwuhon@gmail.com>
2020-11-10 13:09:58 +0100

    ADD: MAquetación - CheckBox

--- app/src/main/java/com/ctbarbanza/gupyou/main/SettingsFragment.java
+++ app/src/main/java/com/ctbarbanza/gupyou/main/SettingsFragment.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.ctbarbanza.gupyou.R;
 import com.ctbarbanza.gupyou.auth.AuthActivity;
@@ -16,22 +19,53 @@ import com.ctbarbanza.gupyou.tools.Settings;
 
 public class SettingsFragment extends Fragment {
 
+
+    private Button btnLogout;
+    private CheckBox check;
+
+    // Editar Perfil de Usuario
+    // Notificaciones On/Off
+    // Cambiar Contraseña
+    // Ver LegalTerms
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.frg_settings, container, false);
         initViews(v);
+        initListeners();
+        updateData();
         return v;
     }
 
-    private void initViews(View v) {
-        Button btn = v.findViewById(R.id.frg_settings_logout_btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+    private void updateData() {
+        boolean isChecked = Settings.init(getActivity()).getBoolean("notifications");
+        check.setChecked(isChecked);
+    }
+
+    private void initListeners() {
+        btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               doLogout();
+                doLogout();
             }
         });
+
+        check.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                Log.d("Check", "Checked: "+isChecked);
+                Settings.init(getActivity()).save("notifications", isChecked);
+            }
+        });
+    }
+
+    private void initViews(View v) {
+
+        check = v.findViewById(R.id.frg_settting_notifications_check);
+        btnLogout = v.findViewById(R.id.frg_settings_logout_btn);
+
     }
 
     private void doLogout() {
