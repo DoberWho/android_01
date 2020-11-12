cad9bf7eae044c48189646c472b26bffb5e748a9
Jorge Parada Fernandez <jwuhon@gmail.com>
2020-11-12 13:48:07 +0100

    ADD: ImeOptions NOTA: Tambien cerramos el Dialog desde una variable de clase

--- app/src/main/java/com/ctbarbanza/gupyou/main/SettingsFragment.java
+++ app/src/main/java/com/ctbarbanza/gupyou/main/SettingsFragment.java
@@ -1,20 +1,29 @@
 package com.ctbarbanza.gupyou.main;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ctbarbanza.gupyou.R;
 import com.ctbarbanza.gupyou.auth.AuthActivity;
@@ -25,6 +34,8 @@ public class SettingsFragment extends Fragment {
 
     private Button btnLogout, btnOthers, btnLegal, btnChangePass;
     private CheckBox check;
+
+    private  AlertDialog dialog;
 
     // Editar Perfil de Usuario
     // Notificaciones On/Off
@@ -100,15 +111,52 @@ public class SettingsFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.dialog_change_pass, null);
         final EditText edtPass = v.findViewById(R.id.dialog_change_pass_pass_edt);
-        EditText edtPassConfirm = v.findViewById(R.id.dialog_change_pass_confirm_edt);
+        final EditText edtPassConfirm = v.findViewById(R.id.dialog_change_pass_confirm_edt);
+
+        edtPassConfirm.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
+
+                boolean action = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    // Cerrar el Teclado
+                    InputMethodManager inputMethodManager = (InputMethodManager) textView.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(textView.getWindowToken(), 0);
+
+                    // Lanzamos la Accion
+                    comprobarPass(edtPass, edtPassConfirm);
+
+                    dialog.dismiss();
+                    action = true;
+                }
+                return action;
+            }
+        });
+
+        // TODO: Ver Esto
+        edtPassConfirm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
         builder.setView(v)
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
 
-                        String pass = edtPass.getText().toString();
-                        String confirm = edtPassConfirm.getText().toString();
-
+                        comprobarPass(edtPass, edtPassConfirm);
                     }
                 }).setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
                     @Override
@@ -117,8 +165,24 @@ public class SettingsFragment extends Fragment {
                     }
                 });
 
-        AlertDialog dialog = builder.create();
+        dialog = builder.create();
         dialog.show();
+    }
+
+    private void comprobarPass(EditText edtPass, EditText edtPassConfirm){
+
+        String pass = edtPass.getText().toString();
+        String confirmacion = edtPassConfirm.getText().toString();
+
+        if (!pass.trim().isEmpty() && pass.equals(confirmacion)){
+            Toast toast = Toast.makeText(getActivity(), R.string.pass_confirm_ok, Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0 , 0);
+            toast.show();
+        }else{
+            Toast toast = Toast.makeText(getActivity(), R.string.pass_confirm_ko, Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0 , 0);
+            toast.show();
+        }
     }
 
 
