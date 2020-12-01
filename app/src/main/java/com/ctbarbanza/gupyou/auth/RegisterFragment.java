fad27cfb81afc89d783e0b842a468317811cf7c5
Jorge Parada Fernandez <jorge.parada@ctbarbanza.com>
2020-12-01 12:46:31 +0100

    ADD: Validacion Formularios

--- app/src/main/java/com/ctbarbanza/gupyou/auth/RegisterFragment.java
+++ app/src/main/java/com/ctbarbanza/gupyou/auth/RegisterFragment.java
@@ -8,17 +8,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.ctbarbanza.gupyou.R;
 import com.ctbarbanza.gupyou.tools.Settings;
+import com.google.android.material.snackbar.Snackbar;
 
 public class RegisterFragment extends Fragment {
+
+    private EditText edtEmail, edtPass, edtConfirm;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.frg_register, container, false);
+        initView(v);
+
+        return v;
+    }
+
+    private void initView(View v) {
         Button btn = v.findViewById(R.id.frg_register_login_btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,11 +41,45 @@ public class RegisterFragment extends Fragment {
         btnAction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goToMain();
+                comprobarFormulario(v);
             }
         });
 
-        return v;
+        edtEmail = v.findViewById(R.id.frg_register_email_edt);
+        edtPass = v.findViewById(R.id.frg_register_password_edt);
+        edtConfirm = v.findViewById(R.id.frg_register_password_confirm_edt);
+    }
+
+
+    private void comprobarFormulario(View v) {
+        String email = edtEmail.getText().toString().trim();
+        String pass = edtPass.getText().toString().trim();
+        String confirm = edtConfirm.getText().toString().trim();
+
+        if (email.isEmpty() || pass.isEmpty() || confirm.isEmpty()){
+            if (email.isEmpty()){
+                String errorMsg = getString(R.string.error_email_empty);
+                edtEmail.setError(errorMsg);
+            }
+            if (pass.isEmpty()){
+                String errorMsg = getString(R.string.error_pass_empty);
+                edtPass.setError(errorMsg);
+            }
+            if (confirm.isEmpty()){
+                String errorMsg = getString(R.string.error_confirm_empty);
+                edtConfirm.setError(errorMsg);
+            }
+            String errorMsg = getString(R.string.error_registro_formulario);
+            Snackbar snack = Snackbar.make(v, errorMsg, Snackbar.LENGTH_LONG);
+            snack.show();
+            return;
+        }
+        if (!pass.equals(confirm)){
+            return;
+        }
+
+
+
     }
 
     private void goToMain() {
